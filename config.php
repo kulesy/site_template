60dcbba0d372497e4ffd4baae62a44a777dc1ede
kulesy <69125238+kulesy@users.noreply.github.com>
2020-08-17 09:51:14 +1200

    Config created
    Created config and added it to my index

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); //change to 'Off' when live
+
+define ("DB_NAME", "gamedata_test");
+define ("DB_USERNAME", "root");
+define ("DB_PASSWORD", "usbw");
+define ("DB_HOST", "localhost");
+
+?>
--- index.php
+++ index.php
@@ -2,30 +2,44 @@
 
 <html lang="en">
 
+<?php
+
+session_start(); // to allow variable transfer between pages...
+include("config.php");
+//Connect to database...
+$dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+if(mysqli_connect_errno()) {
+	echo "Connection failed:" .mysqli_connect_error;
+	exit;
+}
+
+ ?>
+
 	<head>
 		<meta charset="utf-8">
-		
+
 		<!-- For assessment you need to change these -->
 		<meta name="description" content="games, apps, ">
 		<meta name="keywords" content="Game / App Database">
 		<meta name="keywords" content="games, apps, ratings">
-		
+
 		<title>Game Database</title>
-		
+
 		<!-- for multiple fonts change | to %7c * no spaces* -->
 		<link href="http://fonts.googleapis.com/css?family=Lato%7cUbuntu"
 		rel="stylesheet">
-		
+
 		<link rel"stylesheet" href="css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/data_style.css"> <!--custom style sheet -->
 	</head>
-	
+
 	<body>
 		<p class="message">Eek! Your browser does not support grid. 
 		Please upgrade your system.</p>
-		
+
 		<div class="wrapper">
-			
+
 			<!-- logo / small image goes here -->
 			<div class="box logo">
 				<a href="index.php"><img src="images/logo.png"
@@ -33,31 +47,31 @@
 				height="150"
 				alt="Dice" /></a>
 			</div>
-			
+
 			<div class="box banner">
 				<h1>Games Database</h1>
 			</div> <!-- / banner -->
-			
+
 			<div class="box main">
 				<h2>Welcome</h2>
-				
+
 				<p>
 					Put link to original dataset here
 				</p>
 			</div> <!-- / main -->
-			
+
 			<div class="box side">
 				<h2>Search Area</h2>
-				
+
 				<p>
 					This is where the search area goes
 				</p>
 			</div> <!-- / side -->
-			
+
 			<div class="box footer">
 				<p>Mrs Sarah Greeff 2020</p>
 			</div> <!-- / footer -->
-			
+
 		</div> <!-- / wrapper -->
 	</body>
 </html>
