c4ef9c2dc91d7834ed7ad3d4c71a191f8762f47a
kulesy <69125238+kulesy@users.noreply.github.com>
2020-08-17 09:59:31 +1200

    Seperated code
    Seperated code into seperate fies and linked them with php code

---
+++ botbit.php
@@ -0,0 +1,16 @@
+
+			<div class="box side">
+				<h2>Search Area</h2>
+
+				<p>
+					This is where the search area goes
+				</p>
+			</div> <!-- / side -->
+
+			<div class="box footer">
+				<p>Mrs Sarah Greeff 2020</p>
+			</div> <!-- / footer -->
+
+		</div> <!-- / wrapper -->
+	</body>
+</html>
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 'On'); //change to 'Off' when live
 
-define ("DB_NAME", "gamedata_test");
+define ("DB_NAME", "gamedata test");
 define ("DB_USERNAME", "root");
 define ("DB_PASSWORD", "usbw");
 define ("DB_HOST", "localhost");
